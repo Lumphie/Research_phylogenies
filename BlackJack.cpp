35a827f2a1bb11bc053b8d796c69acec50a013ea
Kees <wesselinkkees@gmail.com>
2016-09-08 17:36:04 +0200

    Removed keepPlaying loop
    Couldn't get it to work anymore.

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -157,19 +157,78 @@ int getCardValue(const CardStruct &card)
         if (cardNum.rank == card.rank)
             return cardNum.intRank;
     }
-        std::cout << "Error, rank not found. \n";
-        return -1;
-	}
+    std::cout << "Error, rank not found. \n";
+    return -1;
+}
+
+void playPlayer(CardStruct *cardPtr, int &playerTotal, int &numAcesPlayer)
+{
+    char input;
+    while (playerTotal < 22 && input != 's')
+    {
+        std::cout << "Do you want to hit (h) or stand (s)? ";
+        std::cin >> input;
+        if (input == 's')
+            break;
+
+        playerTotal += getCardValue(*cardPtr);
+        if (getCardValue(*cardPtr) == 11)
+            ++numAcesPlayer;
+
+        std::cout << "\nYou drew a ";
+        printCard(*cardPtr++);
+        std::cout << " and have a total of " << playerTotal << "\n \n";
+
+        if (playerTotal > 21)
+        {
+            if (numAcesPlayer != 0)
+            {
+                playerTotal = playerTotal - 10;
+                --numAcesPlayer;
+            }
+            if (numAcesPlayer ==0)
+                break;
+        }
+    }
+}
+
+void playDealer(CardStruct *cardPtr, int &dealerTotal, int &numAcesDealer, int playerTotal)
+{
+    while (dealerTotal < 22 && dealerTotal < playerTotal)
+    {
+        dealerTotal += getCardValue(*cardPtr);
+        if (getCardValue(*cardPtr) == 11)
+            ++numAcesDealer;
+
+        std::cout << "Dealer drew a ";
+        printCard(*cardPtr++);
+        std::cout << " and has a total of " << dealerTotal << "\n \n";
+
+        if (dealerTotal > 21)
+        {
+            if (numAcesDealer == 0)
+            {
+                dealerTotal = dealerTotal - 10;
+                --numAcesDealer;
+            }
+            else if (numAcesDealer != 0)
+                break;
+        }
+    }
+
+}
 
 bool playBlackJack(std::array<CardStruct, 52> &deck)
 {
-	int dealerTotal = 0;
-	int playerTotal = 0;
-	int numAcesPlayer = 0;
-	int numAcesDealer = 0;
+
 
 	// points a pointer to the first card of the deck
-    const CardStruct *cardPtr = &deck[0];
+    CardStruct *cardPtr = &deck[0];
+
+    int dealerTotal = 0;
+    int playerTotal = 0;
+    int numAcesDealer = 0;
+    int numAcesPlayer = 0;
 
 	// dealer gets a card
 	dealerTotal += getCardValue(*cardPtr);
@@ -191,67 +250,18 @@ bool playBlackJack(std::array<CardStruct, 52> &deck)
 		std::cout << " and have a total of " << playerTotal << "\n \n";
 	}
 
-	std::string input;
 
-	// Gives the player the choice to hit or stand
-	// until the player stands or has a total larger than 21 (and loses)
-	while (true) {
-		std::cout << "\ntype 'h' to hit or 's' to stand ";
-		std::cin >> input;
-		std::cout << "\n";
-		if (input == "s")
-			break;
-
-		if (input == "h"){
-			playerTotal += getCardValue(*cardPtr);
-			if (playerTotal > 21){
-				if (numAcesPlayer != 0){
-					--numAcesPlayer;
-					playerTotal -= 10;
-				}
-			}
-
-			std::cout << "You drew a ";
-			printCard(*cardPtr++);
-			std::cout << " and have a total of " << playerTotal << '\n';
-		}
-
-		// Makes the player lose
-		if (playerTotal > 21)
-			return false;
-	} 
-
+    playPlayer(cardPtr, playerTotal, numAcesPlayer);
+    if (playerTotal > 21)
+        return false;
 	// When the player stands the dealer starts getting cards until he has more than 21 (player wins)
 	// or more than 17 but less than 22 (player loses)
-	if (input == "s")
-	{
-		do {
-			dealerTotal += getCardValue(*cardPtr);
-			
-
-			if (dealerTotal > 21)
-			{
-				if (numAcesDealer != 0)
-				{
-					--numAcesDealer;
-					dealerTotal -= 10;
-				}
-			}
-
-			std::cout << "Dealer drew a ";
-			printCard(*cardPtr++);
-			std::cout << " and has a total of " << dealerTotal << "\n \n";
-
-			if (dealerTotal > 21)
-				return true;
-		} while (dealerTotal < 17 && numAcesDealer == 0 && dealerTotal < playerTotal);
-	}
+    playDealer(cardPtr, dealerTotal, numAcesDealer, playerTotal);
 
 	// if in the end the player has more points than the dealer he wins!
-	if (playerTotal > dealerTotal)
+    if (playerTotal > dealerTotal && !(dealerTotal > 21))
 		return true;
-	else
-		return false;
+    return false;
 }
 
 int main()
@@ -269,14 +279,8 @@ int main()
 			deck[card].rank = static_cast<Cards::Rank>(rank);
 			++card;
 		}
-	
-	bool keepPlaying = true;
-	std::string input;
-	
+		
 	//lets the player play blackjack until he wants to quit.
-	do
-	{
-
 		std::cout << "Let's play a game of Black Jack! \n \n";
 		shuffleDeck(deck);
 		if (playBlackJack(deck))
@@ -287,21 +291,6 @@ int main()
 		{
 			std::cout <<"\nYou Lost! \n" << "Better luck next time! \n \n";
 		}
-
-		std::cout << "Want to play another round?" << '\n' <<
-		"Type 'y' to play again and 'n' to quit. ";
-		std::cin >> input;
-		std::cout << '\n';
-		
-		if (input == "y")
-			keepPlaying = true;
-		if (input == "n")
-		{
-			keepPlaying = false;
-			std::cout << "Thanks for playing! \n";
-		}
-	} while (keepPlaying);
-	
 
 	return 0;
 }
